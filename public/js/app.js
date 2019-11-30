c755668acc437b666007570f787ba64e362c35f6
Tanxy Rogue <tanxyrogue@gmail.com>
2019-11-30 18:59:18 +0200

    created basic empty scene

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+console.log("in app.js");
+
+// A Three.js app requires 3 things a scene, a camera  and a renderer
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
