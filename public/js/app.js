40b1359f8e39eb29e3bd58b970bbf7d55f4752e8
Tanxy Rogue <tanxyrogue@gmail.com>
2019-12-01 08:25:14 +0200

    added on window resize event listener

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,22 @@
-console.log("in app.js");
-
-// A Three.js app requires 3 things a scene, a camera  and a renderer
-
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(
+//#region App initialization ________________
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
 );
 
-var renderer = new THREE.WebGLRenderer();
+let renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+window.addEventListener('resize', () => {
+    // console.log('resizing')
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+//#endregion App Initialization ____________
